44386d47f130a27a1fa04dc8fb1d6a09ef0f3818
koki-develop <kou.pg.0131@gmail.com>
2024-04-07 09:45:16 +0900

    Add SLACK_CHANNEL env var

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,6 +1,10 @@
 import { cleanEnv, str } from "envalid";
 
 export const env = cleanEnv(process.env, {
+  // openai
   OPENAI_API_KEY: str(),
+
+  // slack
+  SLACK_CHANNEL: str(),
   SLACK_OAUTH_TOKEN: str(),
 });
